c6e2d85d258dc7dec4a7bddcb9341a0832ceb579
SlowV <quocviet.hn98@gmail.com>
2024-09-20 00:55:11 +0700

    Add Cache - Redis

--- src/main/java/com/slowv/youtuberef/repository/AccountRepository.java
+++ src/main/java/com/slowv/youtuberef/repository/AccountRepository.java
@@ -1,10 +1,14 @@
 package com.slowv.youtuberef.repository;
 
 import com.slowv.youtuberef.entity.AccountEntity;
+import org.springframework.cache.annotation.Cacheable;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.util.Optional;
 
 public interface AccountRepository extends JpaRepository<AccountEntity, String> {
+    String ACCOUNT_BY_EMAIL_CACHE = "accountByEmail";
+
+    @Cacheable(ACCOUNT_BY_EMAIL_CACHE)
     Optional<AccountEntity> findByUsername(String username);
 }
